edd9f7c4777078a44aed183592206be4e965f226
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-11-16 00:44:18 +0700

    Source

---
+++ src/components/Camera/Camera.js
@@ -0,0 +1,20 @@
+// import React,{useState,useEffect,useRef} from 'react';
+// import classnames from 'classnames';
+
+// import{ detectFaces, drawResults} from'../../helpers/faceApi';
+
+// import Button from'../Button/Button';
+// import Gallery from'../Gallery/Gallery';
+// import Results from'../Results/Results';
+// import Webcam from'react-webcam';
+
+// import './Camera.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// const Camera=({photoMode})=>{
+//     const camera = useRef();
+//     const cameraCanvas = useRef();
+
+//     const [photo,setPhoto] = usrState(undefined);
+    
+// }
