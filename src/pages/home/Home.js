e53e9943d65c93ff35b17688d8516cfeb1c0ae30
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-11-19 15:34:52 +0700

    Source

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,42 +7,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Switch from 'react-switch';
 import Camera from '../../components/Camera/Camera';
 
-import '../../App.css';
+import './Home.css';
 createFaLibrary();
 loadModels();
 function App() {
-  const [mode, setMode] = useState(false); //true = photo mode; false = video mode
+    const [mode, setMode] = useState(false); //true = photo mode; false = video mode
 
-  return (
-    <div className="App">
-      <header>
-        <div className="App__header">
-          <h1>
-            <span>Emotion-Analysis</span>
-          </h1>
-          <div className="App__switcher">
-            <FontAwesomeIcon icon="camera" color={mode ? '#007c6c' : '#cccccc'} />
-            <Switch
-              onChange={() => setMode(!mode)}
-              uncheckedIcon={false}
-              checkedIcon={false}
-              checked={!mode}
-              className="App__switcher-switch"
-            />
-            <FontAwesomeIcon icon="video" color={!mode ? '#007c6c' : '#cccccc'} />
-          </div>
+    return (
+        <div className="App">
+            <header>
+                <div className="App__header">
+                    <h1>
+                        <span>Face detection for emotion</span>
+                    </h1>
+                    <div className="App__switcher">
+                        <FontAwesomeIcon icon="camera" color={mode ? '#007c6c' : '#cccccc'} />
+                        <Switch
+                            onChange={() => setMode(!mode)}
+                            uncheckedIcon={false}
+                            checkedIcon={false}
+                            checked={!mode}
+                            className="App__switcher-switch"
+                        />
+                        <FontAwesomeIcon icon="video" color={!mode ? '#007c6c' : '#cccccc'} />
+                    </div>
+                </div>
+            </header>
+            <Camera photoMode={mode} />
+            <p>
+                Made with{' '}
+                <span role="img" aria-label="heart-emoji">
+                    ❤️
+                </span>{' '}
+                by Rishabh.
+            </p>
         </div>
-      </header>
-      <Camera photoMode={mode} />
-      <p>
-        Made with{' '}
-        <span role="img" aria-label="heart-emoji">
-          ❤️
-        </span>{' '}
-        by Rishabh.
-      </p>
-    </div>
-  );
+    );
 }
 
 export default App;
